36500503c098c34f9b2b1854c40642035134be98
balzbo <celvingaming684@gmail.com>
2022-03-03 06:42:14 +0700

    Alphabot

---
+++ shop/grupbot.js
@@ -0,0 +1,13 @@
+// jangan di ubah jika tidak punya group bot, takutnya eror
+const gcbotwa = () =>{
+	return`Join Aja Semua Fitur Bot Bisa Digunakan !
+
+1. *GRUP SEWABOT*
+https://chat.whatsapp.com/F4lpuwV6BHtIDaSKEKcwOO
+
+
+Jika ada link yang ke reset, silahkan hubungi
+owner untuk meminta link yang baru
+`
+}
+exports.gcbotwa = gcbotwa
---
+++ shop/sewa.js
@@ -0,0 +1,26 @@
+const pc_sewa = () => {
+return `「 *PRICE LIST* 」
+							
+⬣ PRICE SEWABOT
+
+⬡ 1 MINGGU = Rp 2.000
+⬡ 1 BULAN   = Rp 3.000
+⬡ PERMANEN = Rp 5.000
+⬡ PERMANEN + USER PREM = Rp 10.000
+
+
+⬣ PRICE JADIBOT
+
+⬡ JADI BOT PERMANEN 25K
+
+
+⬣ MINAT? PM
+⬡ wa.me/6288227864033
+
+
+NOTE : 
+Bot On 24 Jam Karna Di Run Menggunakan Heroku
+Reset Data Setiap Jam 02:00 `
+	}
+
+exports.pc_sewa = pc_sewa
